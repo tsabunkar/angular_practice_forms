f2e7f1742110496001cd477321dc35d48819f7ca
tsabunkar <tsabunkar@gmail.com>
2018-09-10 13:52:07 +0530

    Template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+
 
   /*  onSubmitOfForm(formElement: HTMLFormElement) {
      console.log('form is submitted !');
@@ -24,11 +22,60 @@ export class AppComponent {
   } */
 
   @ViewChild('FormElem') singupForm: NgForm;
+
   defaultQuestion = 'pet'
+  answerValue = "";
+  genders = ['male', 'female', 'others'];
+
+  userObj = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
+  isFormSubmitted: boolean = false;
+
 
   onSubmitOfForm() { //NgForm-> is Angular form Object which run at backside
     console.log('form is submitted !');
     console.log(this.singupForm);
+    this.userObj.username = this.singupForm.value.userDataGroup.usernameControl
+    this.userObj.email = this.singupForm.value.userDataGroup.emailControl
+    this.userObj.secretQuestion = this.singupForm.value.secretControl
+    this.userObj.answer = this.singupForm.value.questionAnswerControl
+    this.userObj.gender = this.singupForm.value.gender
+    
+    this.isFormSubmitted = true; //form is submitted
+  }
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    /*  
+      this.singupForm.setValue({ //we can set the value of the whole NgForm programmatically using this setValue()
+        userDataGroup: {
+          usernameControl: suggestedName,
+          emailControl: ''
+        },
+        secretControl: 'pet',
+        questionAnswerControl: '',
+        gender: 'male'
+      }); 
+      */
+
+    //?This above approach is not a better approach so using form object with patchValue()
+
+    this.singupForm.form.patchValue({
+      userDataGroup: {
+        usernameControl: suggestedName
+      }
+    })
+
+    //! Thus setValue() -> Is to set the whole NgForm Progrmmatically
+    //! whereas patchValue() -> Is to set the part/patch of fields/controls in NgForm Progrmmatically
 
   }
+
+
 }
